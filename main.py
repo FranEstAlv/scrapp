344ead42ae166d56c3830ecfbe7c21b633551906
Fran Estrada <franciscoesalv@gmail.com>
2026-06-18 17:35:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,6 @@ BOT_TOKEN_2 = os.environ.get("BOT_TOKEN_2", "")
 BOT2_CHAT_ID = os.environ.get("BOT2_CHAT_ID", "")
 
 CHATS_TO_SCRAPE: List[str] = [
-    "-1002408067156",
-    "-1002271492504",
     "-1002328190486"
 ]
 
